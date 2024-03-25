9ff97de8886c0c9d0969d1853ae00bbc5e275219
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2024-03-25 21:08:27 +0530

    initail commit

---
+++ async-hook.js
@@ -0,0 +1,27 @@
+
+
+//Question: Utilize the async_hooks module to trace the execution of asynchronous operations in a Node.js script.
+
+const async_hooks = require('async_hooks');
+
+const hooks = {
+  init(asyncId, type, triggerAsyncId, resource) {
+    console.log(`Init: ${type} with ID ${asyncId}`);
+  },
+  before(asyncId) {
+    console.log(`Before: ${asyncId}`);
+  },
+  after(asyncId) {
+    console.log(`After: ${asyncId}`);
+  },
+  destroy(asyncId) {
+    console.log(`Destroy: ${asyncId}`);
+  }
+};
+
+const asyncHook = async_hooks.createHook(hooks);
+asyncHook.enable();
+
+setTimeout(() => {
+  console.log('Asynchronous operation');
+}, 1000);
