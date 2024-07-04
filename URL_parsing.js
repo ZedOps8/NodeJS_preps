d385d3da6fa9f9d452b70a7270ccfa95191fc341
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2024-07-04 23:24:54 +0530

    Update URL_parsing.js

--- URL_parsing.js
+++ URL_parsing.js
@@ -6,7 +6,3 @@ const url = require('url');
 const urlString = 'https://www.example.com/path?query=value';
 const parsedUrl = url.parse(urlString, true);
 
-console.log(`Protocol: ${parsedUrl.protocol}`);
-console.log(`Host: ${parsedUrl.host}`);
-console.log(`Path: ${parsedUrl.path}`);
-console.log(`Query Parameters: ${JSON.stringify(parsedUrl.query)}`);
