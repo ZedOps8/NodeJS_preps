45c1c9950b2f410e74cb1ee0a3d4017ff9965f68
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2024-07-17 23:51:29 +0530

    Update URL_parsing.js

--- URL_parsing.js
+++ URL_parsing.js
@@ -1,4 +1,9 @@
+//Use the 'url' module to parse a URL and print its components (protocol, host, path, etc.).
 
+const url = require('url');
+
+const urlString = 'https://www.example.com/path?query=value';
+const parsedUrl = url.parse(urlString, true);
 
 
 
